e5f203d35b4f23588f03ab231650883b3fc580fd
unknown <alu0101839905@ull.edu.es>
2026-04-01 16:01:28 +0100

    Commit final de las definiciones

--- Practica_03/definitions.py
+++ Practica_03/definitions.py
@@ -2,8 +2,6 @@ from dagster import Definitions, load_assets_from_modules, load_asset_checks_fro
 from Practica_03 import lab_renta_assets
 from Practica_03 import lab_renta_checks
 
-# Supongamos que tu archivo se llama proyecto_islas.py
-
 defs = Definitions(
     assets=load_assets_from_modules([lab_renta_assets]),
     asset_checks=load_asset_checks_from_modules([lab_renta_checks])
